73546eaef4f4936802800f2e6903afc4ef8fdb74
Mariotti <prog42@ixcsoft.com.br>
2022-11-28 11:07:03 -0300

    FAB na home

--- home.php
+++ home.php
@@ -21,6 +21,10 @@
     </div>
 </section>
 
+<button class="button is-floating is-link">
+    <span class="icon mdi mdi-cart-variant"></span>
+</button>
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="modal.js"></script>
 <script src="carrinho.js"></script>
